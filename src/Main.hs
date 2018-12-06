5408bdbbc87cf370cdb1f051d09d531fbbe389e5
Josenildo Vicente de Araujo <jva@ug4c43.windows.cin.ufpe.br>
2018-12-06 15:01:36 -0300

    movimentação do bloco(personagem) e configuração inicial da pontuação

--- src/Main.hs
+++ src/Main.hs
@@ -17,8 +17,18 @@ data EstadoJogo = Game
     pontos :: Float
  ,  nivel :: Int
  ,  texto :: String
- , comecar :: Bool
+ ,  start :: Bool
+ ,  posicaoBloco :: Float
  }
+
+toInt :: Float -> Int
+toInt x = round x
+
+posBlocoLim :: Float -> Float
+posBlocoLim x 
+ | x >= 400 = 390
+ | x <= (-400) = (-390)
+ | otherwise = x
 
 window :: Display
 window = InWindow "Dodger" (width,height) (offset,offset)
@@ -28,7 +38,7 @@ background = white
 
 drawing :: EstadoJogo -> Picture
 drawing game 
- | comecar game == False = menu game
+ | start game == False = menu game
  | otherwise = estadoRodando game
 
 estadoRodando :: EstadoJogo -> Picture
@@ -38,15 +48,17 @@ estadoRodando game = pictures [
     ]
     where
         bloco = 
-            translate (-100) (-200) $
+            translate (posicaoBloco game) (-200) $
             Color blue $
             rectangleSolid 50 50
         
+        pontuacao = toInt ((pontos game) / 10)
+
         pontoAtual =
             translate 280 180 $
             Scale 0.5 0.5 $
             Color red $
-            Text (show (pontos game)) 
+            Text (show pontuacao) 
 
 menu :: EstadoJogo -> Picture
 menu game = pictures [
@@ -80,12 +92,16 @@ estadoInicial = Game {
        pontos = 0
     ,  nivel = 1
     , texto = "pontuacao = 0"
-    , comecar = False
+    , start = False
+    , posicaoBloco = (-150)
     }
 
 
 evento :: Event -> EstadoJogo -> EstadoJogo
-evento (EventKey (SpecialKey KeySpace) _ _ _) game = game {comecar = True}
+evento (EventKey (SpecialKey KeySpace) _ _ _) game = game {posicaoBloco = 0,start = True}
+evento (EventKey (Char 'p') _ _ _) game = game {start = False}
+evento (EventKey (SpecialKey KeyLeft) _ _ _) game = game { posicaoBloco = posBlocoLim(((posicaoBloco game) - 10))}
+evento (EventKey (SpecialKey KeyRight) _ _ _) game = game { posicaoBloco = posBlocoLim(((posicaoBloco game) + 10))}
 evento _ game = game
 
 atualizar :: Float -> EstadoJogo -> EstadoJogo
